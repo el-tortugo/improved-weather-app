eface4f6699236c0f9b87bbb132e103946224a0e
Kehinde Adeoso <kennolite@gmail.com>
2023-10-27 19:54:59 -0400

    Adjusted formatting of <div>

--- kehinde-improved-weather-app/src/App.jsx
+++ kehinde-improved-weather-app/src/App.jsx
@@ -3,14 +3,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import CityInput from './components/city-search/city-search'
 import './App.css'
-import { CityProvider } from './context/CityContext';
+import { CityProvider } from './context/ci-context';
 
 function App() {
   const [city, setCity] = React.useState('');
 
   return (
     <CityProvider value={{ city, setCity }}>
-          <div>
+    <div>
       <CityInput onCityChange={handleCityChange} />
       {/* Render other components here */}
       {city && <h1>Weather for {city}</h1>}
